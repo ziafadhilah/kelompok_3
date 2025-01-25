60337d7ff1b7004e9fcee75c6e77be40cb346009
brodap <dafaiklashul@gmail.com>
2025-01-24 21:50:30 -0800

    nambah bosstrap

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Ini home</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-    <a href="/home">Home</a>
-    <a href="/about">About</a>
-    <h1>"Halaman home"</h1>
+@extends('layouts.main')
+@section('content')
+<div class="container text-danger">
+"Halaman home"
+</div>
+<h1></h1>
     <h3> Owner : {{ $nama }} </h3>
 
-    <script src="js/script.js"></script>
-</body>
-</html>
+@endsection
